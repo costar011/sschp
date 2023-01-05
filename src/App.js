06d8da4751d7aab0bb511ad2fdf64c26fafe75a0
costar011 <dnaer456@gmail.com>
2023-01-05 17:36:22 +0900

    react setting

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./header/Header";
+import Content from "./content/Content";
+import Footer from "./footer/Footer";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/content/Content.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Content.css";
+
+const Content = () => {
+  return (
+    <div>
+      <h1>main</h1>
+    </div>
+  );
+};
+
+export default Content;
